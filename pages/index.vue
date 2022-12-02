4baec46ae1b453f997c9a0f17a35bf4e92a614df
yvon RAKOTONANAHARY <yrakotonanahary@equad.fr>
2022-12-02 13:06:04 +0100

    usecolore

---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>Color mode: {{ $colorMode.value }}</h1>
+    <select v-model="$colorMode.preference">
+      <option value="system">System</option>
+      <option value="light">Light</option>
+      <option value="dark">Dark</option>
+      <option value="sepia">Sepia</option>
+    </select>
+    <p class="p-2 text-lg">Hello world</p>
+  </div>
+</template>
+
+<script setup>
+  const colorMode = useColorMode()
+  console.log(colorMode.preference)
+</script>
+
+<style scoped>
+body {
+  background-color: #fff;
+  color: rgba(0,0,0,0.8);
+}
+.dark-mode body {
+  background-color: #091a28;
+  color: #ebf4f1;
+}
+.sepia-mode body {
+  background-color: #f1e7d0;
+  color: #433422;
+}
+</style>
